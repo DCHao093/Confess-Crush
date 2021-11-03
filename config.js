2f33ccc118a356336d0a399322613d681633c1b2
DCHao093 <93647294+DCHao093@users.noreply.github.com>
2021-11-03 16:34:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Mình cũng yêu bạn nhiều lắm :333',
-    mess: 'Mình biết mà :)). Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà :)). Yêu bạn nhiều nhiều ��',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Ok lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
